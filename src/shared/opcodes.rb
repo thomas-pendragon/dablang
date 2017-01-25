984beb3ddef69803459b169253520189ee98392c
Thomas Pendragon <root@thomaspendragon.com>
2017-01-25 18:00:15 +0100

    Opcodes description in single place

--- src/shared/opcodes.rb
+++ src/shared/opcodes.rb
@@ -1,11 +1,11 @@
 OPCODES = {
-  0x00 => {name: 'START_FUNCTION', args: %i(vlc uint16 uint16)},
-  0x01 => {name: 'CONSTANT_SYMBOL', arg: :vlc},
-  0x02 => {name: 'CONSTANT_STRING', arg: :vlc},
-  0x03 => {name: 'PUSH_CONSTANT', arg: :uint16},
-  0x04 => {name: 'CALL', arg: :uint16},
-  0x06 => {name: 'SET_VAR', arg: :uint16},
-  0x07 => {name: 'VAR', arg: :uint16},
+  0x00 => {name: 'START_FUNCTION', args: %i(vlc uint16 uint16)}, # function name, number of local variables, body length
+  0x01 => {name: 'CONSTANT_SYMBOL', arg: :vlc}, # symbol
+  0x02 => {name: 'CONSTANT_STRING', arg: :vlc}, # string
+  0x03 => {name: 'PUSH_CONSTANT', arg: :uint16}, # constant index, push(1)
+  0x04 => {name: 'CALL', arg: :uint16}, # n = number of arguments, pop(n + 1)
+  0x05 => {name: 'SET_VAR', arg: :uint16}, # local variable index, pop(1)
+  0x06 => {name: 'VAR', arg: :uint16}, # local variable index, push(1)
 }.freeze
 
 OPCODES_REV = OPCODES.map { |k, v| [v[:name], v.merge(opcode: k)] }.to_h
