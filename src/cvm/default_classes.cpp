827b117a6317b5f66d57be4f61c9d0c3e67da3c4
Thomas Pendragon <root@thomaspendragon.com>
2017-02-23 18:56:43 +0100

    VM: default classes refactor

--- src/cvm/default_classes.cpp
+++ src/cvm/default_classes.cpp
@@ -14,137 +14,70 @@ DabClass &DabVM::define_builtin_class(const std::string &name, size_t class_inde
 
 void DabVM::define_default_classes()
 {
-    DabClass object_class;
-    object_class.index   = CLASS_OBJECT;
-    object_class.name    = "Object";
-    object_class.builtin = true;
-    {
-        DabFunction fun;
-        fun.name    = "new";
-        fun.regular = false;
-        fun.extra   = [this](size_t n_args, size_t n_ret) {
-            assert(n_args == 1);
-            assert(n_ret == 1);
+    auto &object_class = define_builtin_class("Object", CLASS_OBJECT);
+    object_class.add_static_function("new", [this](size_t n_args, size_t n_ret) {
+        assert(n_args == 1);
+        assert(n_ret == 1);
+        auto arg = stack.pop_value();
+        assert(arg.type == TYPE_CLASS);
+        stack.push_value(arg.create_instance());
+    });
+    object_class.add_function("class", [this](size_t n_args, size_t n_ret) {
+        assert(n_args == 1);
+        assert(n_ret == 1);
+        auto arg = stack.pop_value();
+        stack.push_value(arg.get_class(*this));
+    });
+
+    auto &string_class = define_builtin_class("String", CLASS_STRING);
+    string_class.add_function("upcase", [this](size_t n_args, size_t n_ret) {
+        assert(n_args == 1);
+        assert(n_ret == 1);
+        auto arg0 = stack.pop_value();
+        assert(arg0.type == TYPE_STRING);
+        auto &s = arg0.string;
+        std::transform(s.begin(), s.end(), s.begin(), ::toupper);
+        stack.push(arg0);
+    });
+    string_class.add_static_function("new", [this](size_t n_args, size_t n_ret) {
+        assert(n_args == 1 || n_args == 2);
+        assert(n_ret == 1);
+        auto     klass = stack.pop_value();
+        DabValue ret_value;
+        ret_value.type = TYPE_STRING;
+        ret_value.kind = VAL_STACK;
+        if (n_args == 2)
+        {
             auto arg = stack.pop_value();
-            assert(arg.type == TYPE_CLASS);
-            stack.push_value(arg.create_instance());
-        };
-        object_class.static_functions["new"] = fun;
-    }
-    {
-        DabFunction fun;
-        fun.name    = "class";
-        fun.regular = false;
-        fun.extra   = [this](size_t n_args, size_t n_ret) {
-            assert(n_args == 1);
-            assert(n_ret == 1);
+            assert(arg.type == TYPE_STRING);
+            ret_value.string = arg.string;
+        }
+        stack.push_value(ret_value);
+    });
+
+    define_builtin_class("LiteralString", CLASS_LITERALSTRING, CLASS_STRING);
+
+    auto &fixnum_class = define_builtin_class("Fixnum", CLASS_FIXNUM);
+    fixnum_class.add_static_function("new", [this](size_t n_args, size_t n_ret) {
+        assert(n_args == 1 || n_args == 2);
+        assert(n_ret == 1);
+        auto     klass = stack.pop_value();
+        DabValue ret_value;
+        ret_value.type   = TYPE_FIXNUM;
+        ret_value.kind   = VAL_STACK;
+        ret_value.fixnum = 0;
+        if (n_args == 2)
+        {
             auto arg = stack.pop_value();
-            stack.push_value(arg.get_class(*this));
-        };
-        object_class.functions["class"] = fun;
-    }
-    classes[CLASS_OBJECT] = object_class;
-
-    {
-        DabClass klass;
-        klass.index   = CLASS_STRING;
-        klass.name    = "String";
-        klass.builtin = true;
-        {
-            DabFunction fun;
-            fun.name    = "upcase";
-            fun.regular = false;
-            fun.extra   = [this](size_t n_args, size_t n_ret) {
-                assert(n_args == 1);
-                assert(n_ret == 1);
-                auto arg0 = stack.pop_value();
-                assert(arg0.type == TYPE_STRING);
-                auto &s = arg0.string;
-                std::transform(s.begin(), s.end(), s.begin(), ::toupper);
-                stack.push(arg0);
-            };
-            klass.functions["upcase"] = fun;
+            assert(arg.type == TYPE_FIXNUM);
+            ret_value.fixnum = arg.fixnum;
         }
-        {
-            DabFunction fun;
-            fun.name    = "new";
-            fun.regular = false;
-            fun.extra   = [this](size_t n_args, size_t n_ret) {
-                assert(n_args == 1 || n_args == 2);
-                assert(n_ret == 1);
-                auto     klass = stack.pop_value();
-                DabValue ret_value;
-                ret_value.type = TYPE_STRING;
-                ret_value.kind = VAL_STACK;
-                if (n_args == 2)
-                {
-                    auto arg = stack.pop_value();
-                    assert(arg.type == TYPE_STRING);
-                    ret_value.string = arg.string;
-                }
-                stack.push_value(ret_value);
-            };
-            klass.static_functions["new"] = fun;
-        }
-        classes[CLASS_STRING] = klass;
-    }
+        stack.push_value(ret_value);
+    });
 
-    DabClass literal_string_class;
-    literal_string_class.index            = CLASS_LITERALSTRING;
-    literal_string_class.name             = "LiteralString";
-    literal_string_class.builtin          = true;
-    literal_string_class.superclass_index = CLASS_STRING;
-    classes[CLASS_LITERALSTRING]          = literal_string_class;
+    define_builtin_class("LiteralFixnum", CLASS_LITERALFIXNUM, CLASS_FIXNUM);
 
-    {
-        DabClass klass;
-        klass.index   = CLASS_FIXNUM;
-        klass.name    = "Fixnum";
-        klass.builtin = true;
-        {
-            DabFunction fun;
-            fun.name    = "new";
-            fun.regular = false;
-            fun.extra   = [this](size_t n_args, size_t n_ret) {
-                assert(n_args == 1 || n_args == 2);
-                assert(n_ret == 1);
-                auto     klass = stack.pop_value();
-                DabValue ret_value;
-                ret_value.type   = TYPE_FIXNUM;
-                ret_value.kind   = VAL_STACK;
-                ret_value.fixnum = 0;
-                if (n_args == 2)
-                {
-                    auto arg = stack.pop_value();
-                    assert(arg.type == TYPE_FIXNUM);
-                    ret_value.fixnum = arg.fixnum;
-                }
-                stack.push_value(ret_value);
-            };
-            klass.static_functions["new"] = fun;
-        }
-        classes[CLASS_FIXNUM] = klass;
-    }
+    define_builtin_class("Boolean", CLASS_BOOLEAN);
 
-    DabClass literal_fixnum_class;
-    literal_fixnum_class.index            = CLASS_LITERALFIXNUM;
-    literal_fixnum_class.name             = "LiteralFixnum";
-    literal_fixnum_class.builtin          = true;
-    literal_fixnum_class.superclass_index = CLASS_FIXNUM;
-    classes[CLASS_LITERALFIXNUM]          = literal_fixnum_class;
-
-    DabClass boolean_class;
-    boolean_class.index    = CLASS_BOOLEAN;
-    boolean_class.name     = "Boolean";
-    boolean_class.builtin  = true;
-    classes[CLASS_BOOLEAN] = boolean_class;
-
-    {
-        DabClass klass;
-        klass.index                   = CLASS_LITERALBOOLEAN;
-        klass.name                    = "LiteralBoolean";
-        klass.builtin                 = true;
-        klass.superclass_index        = CLASS_BOOLEAN;
-        classes[CLASS_LITERALBOOLEAN] = klass;
-    }
+    define_builtin_class("LiteralBoolean", CLASS_LITERALBOOLEAN, CLASS_BOOLEAN);
 }
