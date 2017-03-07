e8a5e348a44944fa39219bffb0cbdabf299143bd
Thomas Pendragon <root@thomaspendragon.com>
2017-03-07 22:38:43 +0100

    build: premake can build multiple projects

--- premake5.lua
+++ premake5.lua
@@ -2,21 +2,26 @@ workspace "Dab"
   location "build"
   configurations { "Debug", "Release" }
 
-project "cvm"
-  kind "ConsoleApp"
-  language "C++"
-  targetdir "bin/"
-  buildoptions "-std=c++11"
+function dab_common_setup(name)
+  project(name)
+    kind "ConsoleApp"
+    language "C++"
+    targetdir "bin/"
+    buildoptions "-std=c++11"
 
-  files { "src/cvm/**.h", "src/cvm/**.cpp" }
+    files { "src/cshared/**.h", "src/cshared/**.cpp" }
+    files { "src/"..name.."/**.h", "src/"..name.."/**.cpp" }
 
-  filter "configurations:Debug"
-    defines { "DEBUG" } 
-    symbols "On"
+    filter "configurations:Debug"
+      defines { "DEBUG" } 
+      symbols "On"
 
-  filter "configurations:Release"
-    optimize "On"
+    filter "configurations:Release"
+      optimize "On"
 
-  filter "action:xcode4"
-    buildoptions "-stdlib=libc++"
-    linkoptions "-stdlib=libc++"
+    filter "action:xcode4"
+      buildoptions "-stdlib=libc++"
+      linkoptions "-stdlib=libc++"
+end
+
+dab_common_setup("cvm")
