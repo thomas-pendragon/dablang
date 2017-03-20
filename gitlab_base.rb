32c69453540610ecb407968862c5d2ad80f09f1e
Thomas Pendragon <root@thomaspendragon.com>
2017-03-20 18:16:46 +0100

    CI: use only a single version of g++ and clang in CI to reduce build times

--- gitlab_base.rb
+++ gitlab_base.rb
@@ -6,15 +6,15 @@ data = YAML.load_file(base_path)
 
 compilers = [
   'g++-4.7',
-  'g++-4.8',
-  'g++-4.9',
-  'g++-5',
-  'g++-6',
+  # 'g++-4.8',
+  # 'g++-4.9',
+  # 'g++-5',
+  # 'g++-6',
   'clang++-3.5',
-  'clang++-3.6',
-  'clang++-3.7',
-  'clang++-3.8',
-  'clang++-3.9',
+  # 'clang++-3.6',
+  # 'clang++-3.7',
+  # 'clang++-3.8',
+  # 'clang++-3.9',
 ]
 
 compilers = compilers.map do |compiler|
