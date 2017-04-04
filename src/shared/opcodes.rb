ed201fc257f0395ad0a57a88523977dc390ba091
Thomas Pendragon <root@thomaspendragon.com>
2017-04-04 19:56:56 +0200

    opcodes: change LOAD_FUNCTION first arg to 16bit (relative offset, like JMP)

--- src/shared/opcodes.rb
+++ src/shared/opcodes.rb
@@ -24,7 +24,7 @@ OPCODES = {
   0x16 => {name: 'PUSH_TRUE'}, # push(1)
   0x17 => {name: 'PUSH_FALSE'}, # push(1)
   0x18 => {name: 'BREAK_LOAD'}, # stop loading the code
-  0x19 => {name: 'LOAD_FUNCTION', args: %i(vlc uint64 uint64 uint16 uint16)}, # [name, address, sizeof, classIndex, nLocals]
+  0x19 => {name: 'LOAD_FUNCTION', args: %i(uint16 vlc uint16 uint16)}, # [address, name, classIndex, nLocals]
   0x20 => {name: 'DEFINE_CLASS', args: %i(vlc uint16)}, # n = name, n2 = base class index
 }.freeze
 
