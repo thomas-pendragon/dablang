77ccccf699dfa307f42ecec2be72da0abc6b207b
Thomas Pendragon <root@thomaspendragon.com>
2017-04-11 20:34:53 +0200

    New tests: .dab to assembly

--- Rakefile
+++ Rakefile
@@ -14,6 +14,9 @@ vm_outputs = []
 disasm_inputs = Dir.glob('test/disasm/*.dat').sort.reverse
 disasm_outputs = []
 
+asm_inputs = Dir.glob('test/asm/*.asmt').sort.reverse
+asm_outputs = []
+
 sources = Dir.glob('src/**/*.rb')
 
 makefile = 'build/Makefile'
@@ -100,6 +103,14 @@ disasm_inputs.each do |input_test_file|
   end
 end
 
+asm_inputs.each do |input_test_file|
+  output_output_file = input_test_file.gsub('test/asm/', 'tmp/test_asm_').gsub('.asmt', '.out')
+  asm_outputs << output_output_file
+  file output_output_file => sources + [input_test_file] do
+    psystem("ruby src/frontend/frontend_asm.rb #{input_test_file} --test_output_prefix test_asm_ --test_output_dir ./tmp/")
+  end
+end
+
 gitlab = '.gitlab-ci.yml'
 gitlab_base = 'gitlab_base.rb'
 
@@ -122,6 +133,9 @@ task vm_spec: vm_outputs do
 end
 
 task disasm_spec: disasm_outputs do
+end
+
+task asm_spec: asm_outputs do
 end
 
 task reverse: outputs.reverse
---
+++ src/frontend/frontend_asm.rb
@@ -0,0 +1,49 @@
+require_relative './shared.rb'
+
+def read_test_file(fname)
+  base_read_test_file(fname)
+end
+
+def extract_format_source(input, output)
+  describe_action(input, output, 'extract source') do
+    text = read_test_file(input)[:code]
+    File.open(output, 'wb') do |file|
+      file << text
+    end
+  end
+end
+
+def compile(input, output)
+  run_ruby_part(input, output, 'compile', 'compiler', '--raw')
+end
+
+def run_test(settings)
+  input = settings[:input]
+  test_output_dir = settings[:test_output_dir] || '.'
+  test_prefix = settings[:test_output_prefix] || ''
+
+  data = read_test_file(input)
+
+  info = "Running test #{input.blue.bold} in directory #{test_output_dir.blue.bold}..."
+  puts info
+  FileUtils.mkdir_p(test_output_dir)
+
+  dab = Pathname.new(test_output_dir).join(test_prefix + File.basename(input).ext('.dab')).to_s
+  asm = Pathname.new(test_output_dir).join(test_prefix + File.basename(input).ext('.asm')).to_s
+  out = Pathname.new(test_output_dir).join(test_prefix + File.basename(input).ext('.out')).to_s
+  FileUtils.rm(out) if File.exist?(out)
+
+  extract_format_source(input, dab)
+  compile(dab, asm)
+
+  expected = data[:expect].strip
+
+  actual = File.read(asm).strip
+  compare_output(info, actual, expected)
+
+  File.open(out, 'wb') { |f| f << '1' }
+end
+
+if $settings[:input].downcase.end_with? '.asmt'
+  run_test($settings)
+end
