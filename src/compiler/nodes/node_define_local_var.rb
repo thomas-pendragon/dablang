2b097addd9ade06d51b5612a060dc0fdba848306
Thomas Pendragon <root@thomaspendragon.com>
2017-05-10 15:24:36 +0200

    compiler: remove no longer used method DabNodeDefineLocalVar.var_uses

--- src/compiler/nodes/node_define_local_var.rb
+++ src/compiler/nodes/node_define_local_var.rb
@@ -1,18 +1,6 @@
 require_relative 'node_set_local_var.rb'
 
 class DabNodeDefineLocalVar < DabNodeSetLocalVar
-  def var_uses
-    ret = []
-    function.visit_all(DabNodeSetLocalVar) do |local_var|
-      next if local_var == self
-      ret << local_var if local_var.identifier == self.identifier
-    end
-    function.visit_all(DabNodeLocalVar) do |local_var|
-      ret << local_var if local_var.var_definition == self
-    end
-    ret
-  end
-
   def formatted_source(options)
     'var ' + super
   end
