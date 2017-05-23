638e4ca5cd443145ddd5faf4786e9604b73d156e
Thomas Pendragon <root@thomaspendragon.com>
2017-05-23 10:05:58 +0200

    compiler: simplify concreteify conditions

--- src/compiler/processors/concreteify_call.rb
+++ src/compiler/processors/concreteify_call.rb
@@ -7,8 +7,10 @@ class ConcreteifyCall
 
   def all_args_concrete?(node)
     return false if node.target_function == true
-    # TODO: WIP, run check first
-    (node.args.count > 0) && (node.target_function.arglist.to_a.all? { |arg| arg.my_type.is_a? DabTypeAny }) && (node.target_function.argcount == node.args.count) && (node.args.all? { |arg| arg.my_type.concrete? })
+    return false if node.args.count == 0
+    return false unless node.target_function.arglist.to_a.all? { |arg| arg.my_type.is_a? DabTypeAny }
+    return false unless node.args.all? { |arg| arg.my_type.concrete? }
+    true
   end
 
   def concreteify_call!(node)
