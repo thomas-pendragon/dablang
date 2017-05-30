7d61a150d47c82638476c3eb0b93d49d745268b0
Thomas Pendragon <root@thomaspendragon.com>
2017-05-30 19:32:55 +0200

    compiler: CallBlock accepts optional arglist

--- src/compiler/nodes/node_call_block.rb
+++ src/compiler/nodes/node_call_block.rb
@@ -1,12 +1,17 @@
 require_relative 'node.rb'
 
 class DabNodeCallBlock < DabNode
-  def initialize(body)
+  def initialize(body, arglist = nil)
     super()
-    insert(body)
+    insert(body, 'body')
+    insert(arglist, 'arglist') if arglist
   end
 
   def body
     children[0]
+  end
+
+  def arglist
+    children[1]
   end
 end
