96be402cf1bc57c72e32a2ef7a238461b48e2073
Thomas Pendragon <root@thomaspendragon.com>
2017-06-04 14:01:37 +0200

    vm: use class_name in Class.to_s

--- src/cvm/default_classes.cpp
+++ src/cvm/default_classes.cpp
@@ -27,8 +27,8 @@ void DabVM::define_default_classes()
     });
     object_class.add_simple_function(vm, "class",
                                      [this](DabValue self) { return self.get_class(*this); });
-    object_class.add_simple_function(
-        vm, "to_s", [this](DabValue self) { return "#" + self.get_class(*this).name; });
+    object_class.add_simple_function(vm, "to_s",
+                                     [this](DabValue self) { return "#" + self.class_name(); });
     object_class.add_simple_function(vm, "__destruct", [this](DabValue) { return nullptr; });
     object_class.add_static_function("to_s", [this](size_t n_args, size_t n_ret, void *blockaddr) {
         assert(blockaddr == 0);
