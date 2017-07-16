af2ed5d7ce2c0854d8a781398c5cc7a25422560b
Thomas Pendragon <root@thomaspendragon.com>
2017-07-16 12:22:44 +0200

    compiler: allow Object->Int32 cast

--- src/compiler/parts/types.rb
+++ src/compiler/parts/types.rb
@@ -126,7 +126,7 @@ class DabTypeInt32 < DabTypeFixnum
   end
 
   def can_assign_from?(other_type)
-    other_type.is_a? DabTypeFixnum
+    other_type.is_a?(DabTypeFixnum) || other_type.is_a?(DabTypeObject)
   end
 
   def requires_cast?(other_type)
