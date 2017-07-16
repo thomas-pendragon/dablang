36e6d531cca53b3ae346d7d90abbbedfc6f340bb
Thomas Pendragon <root@thomaspendragon.com>
2017-07-17 00:32:44 +0200

    vm: separate classes.h

---
+++ src/cshared/classes.h
@@ -0,0 +1,16 @@
+enum
+{
+    CLASS_OBJECT        = 0,
+    CLASS_STRING        = 1,
+    CLASS_LITERALSTRING = 2,
+    CLASS_FIXNUM        = 3,
+    CLASS_LITERALFIXNUM = 4,
+    CLASS_BOOLEAN       = 5,
+    CLASS_NILCLASS      = 6,
+    CLASS_ARRAY         = 7,
+    CLASS_UINT8         = 8,
+    CLASS_INT32         = 9,
+    CLASS_METHOD        = 10,
+
+    CLASS_INT_SYMBOL = 0xFE,
+};
--- src/cvm/cvm.h
+++ src/cvm/cvm.h
@@ -111,22 +111,7 @@ enum
     TYPE_METHOD,
 };
 
-enum
-{
-    CLASS_OBJECT        = 0,
-    CLASS_STRING        = 1,
-    CLASS_LITERALSTRING = 2,
-    CLASS_FIXNUM        = 3,
-    CLASS_LITERALFIXNUM = 4,
-    CLASS_BOOLEAN       = 5,
-    CLASS_NILCLASS      = 6,
-    CLASS_ARRAY         = 7,
-    CLASS_UINT8         = 8,
-    CLASS_INT32         = 9,
-    CLASS_METHOD        = 10,
-
-    CLASS_INT_SYMBOL = 0xFE,
-};
+#include "../cshared/classes.h"
 
 enum
 {
