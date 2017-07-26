9a80e7a467e67da26a93cb5de1785425fbc9be6d
Thomas Pendragon <root@thomaspendragon.com>
2017-07-26 14:59:51 +0200

    spec: DabNode.replace_child spec

---
+++ spec/compiler/replace_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+require_relative '../../src/compiler/_requires.rb'
+
+describe DabNode do
+  it 'allows to replace child' do
+    body = DabNodeCodeBlock.new
+    p1 = DabNodeSymbol.new(:p1)
+    p2 = DabNodeSymbol.new(:p2)
+    p3 = DabNodeSymbol.new(:p3)
+    body << p1 << p2
+    body.replace_child(p2, p3)
+    expect(body.all_nodes(DabNodeSymbol).map(&:symbol)).to eq(%i[p1 p3])
+  end
+
+  it 'allows to replace child with array' do
+    body = DabNodeCodeBlock.new
+    p1 = DabNodeSymbol.new(:p1)
+    p2 = DabNodeSymbol.new(:p2)
+    p3 = DabNodeSymbol.new(:p3)
+    p4 = DabNodeSymbol.new(:p4)
+    p5 = DabNodeSymbol.new(:p5)
+    body << p1 << p2 << p5
+    body.replace_child(p2, [p3, p4])
+    expect(body.all_nodes(DabNodeSymbol).map(&:symbol)).to eq(%i[p1 p3 p4 p5])
+  end
+
+  it 'allows to replace child with array with the child itself' do
+    body = DabNodeCodeBlock.new
+    p1 = DabNodeSymbol.new(:p1)
+    p2 = DabNodeSymbol.new(:p2)
+    p3 = DabNodeSymbol.new(:p3)
+    p4 = DabNodeSymbol.new(:p4)
+    body << p1 << p2 << p4
+    body.replace_child(p2, [p2, p3])
+    expect(body.all_nodes(DabNodeSymbol).map(&:symbol)).to eq(%i[p1 p2 p3 p4])
+  end
+end
