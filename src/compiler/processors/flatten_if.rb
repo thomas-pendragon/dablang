1bf223a5e5956027b563929bda6edbc3180cd639
Thomas Pendragon <root@thomaspendragon.com>
2017-07-26 13:26:18 +0200

    compiler: store nodes locally in FlattenIf

--- src/compiler/processors/flatten_if.rb
+++ src/compiler/processors/flatten_if.rb
@@ -1,12 +1,14 @@
 class FlattenIf
   def run(node)
     node.parent.splice(node) do |continue_block|
-      node.if_true.insert(DabNodeJump.new(continue_block))
-      node.if_false&.insert(DabNodeJump.new(continue_block))
+      if_true = node.if_true
+      if_false = node.if_false
+      if_true.insert(DabNodeJump.new(continue_block))
+      if_false&.insert(DabNodeJump.new(continue_block))
       condition_block = DabNodeCodeBlock.new
-      ifjump = DabNodeConditionalJump.new(node.condition, node.if_true, node.if_false || continue_block)
+      ifjump = DabNodeConditionalJump.new(node.condition, if_true, if_false || continue_block)
       condition_block.insert(ifjump)
-      [condition_block, node.if_true, node.if_false].compact
+      [condition_block, if_true, if_false].compact
     end
     true
   end
