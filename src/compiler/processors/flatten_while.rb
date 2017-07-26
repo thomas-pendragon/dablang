46d466ac7a9048b9cacf480727e68f6221942ce1
Thomas Pendragon <root@thomaspendragon.com>
2017-07-26 13:19:26 +0200

    compiler: store on_block locally in FlattenWhile

--- src/compiler/processors/flatten_while.rb
+++ src/compiler/processors/flatten_while.rb
@@ -2,10 +2,11 @@ class FlattenWhile
   def run(node)
     node.parent.splice(node) do |continue_block|
       condition_block = DabNodeCodeBlock.new
-      ifjump = DabNodeConditionalJump.new(node.condition, node.on_block, continue_block)
-      node.on_block.insert(DabNodeJump.new(condition_block))
+      on_block = node.on_block
+      ifjump = DabNodeConditionalJump.new(node.condition, on_block, continue_block)
+      on_block.insert(DabNodeJump.new(condition_block))
       condition_block.insert(ifjump)
-      [condition_block, node.on_block]
+      [condition_block, on_block]
     end
     true
   end
