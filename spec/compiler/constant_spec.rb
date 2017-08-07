9f1f5fac0d735f540dcf751f1ab895cbbbefb6a5
Thomas Pendragon <root@thomaspendragon.com>
2017-08-07 12:36:46 +0200

    constant reference tracking spec

---
+++ spec/compiler/constant_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+require_relative '../../src/compiler/_requires.rb'
+
+describe DabNodeConstant do
+  it 'tracks references' do
+    parent = DabNodeCodeBlock.new
+    function = DabNodeFunction.new('test', parent, nil)
+    unit = DabNodeUnit.new
+    unit.add_function(function)
+
+    value = DabNodeLiteralString.new('foo')
+
+    use1 = unit.add_constant(value)
+    const = use1.target
+
+    parent << use1
+
+    expect(const.references).to match_array [use1]
+
+    use2 = unit.add_constant(const)
+    parent << use2
+
+    expect(const.references).to match_array [use1, use2]
+  end
+end
