2cca92b6fde0cfc3ce1a6b621ed4e2b7f5685e70
Thomas Pendragon <root@thomaspendragon.com>
2017-08-25 16:32:32 +0200

    compiler: Uncomplexify uses SSA regs directly

--- src/compiler/processors/uncomplexify.rb
+++ src/compiler/processors/uncomplexify.rb
@@ -3,8 +3,9 @@ class Uncomplexify
     return unless complex_arg = node.uncomplexify_args.detect(&:complex?)
     id = node.function.allocate_tempvar
     arg_dup = complex_arg.dup
-    setter = DabNodeDefineLocalVar.new(id, arg_dup)
-    getter = DabNodeLocalVar.new(id)
+    reg = node.function.allocate_ssa
+    setter = DabNodeSSASet.new(arg_dup, reg, id)
+    getter = DabNodeSSAGet.new(reg, id)
     node.prepend_instruction(setter)
     complex_arg.replace_with!(getter)
     true
