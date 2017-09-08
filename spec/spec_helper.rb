7a5bffb3c4c567a2f792c08efcba3095fcd64f69
Thomas Pendragon <root@thomaspendragon.com>
2017-09-08 13:02:06 +0200

    spec: add dummy dab_benchmark support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require_relative '../setup.rb'
 
+def dab_benchmark(*_)
+  yield
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
