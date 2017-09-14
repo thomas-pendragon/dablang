93f39d6f34308695655ba9dccf79b3e95ca99725
Thomas Pendragon <root@thomaspendragon.com>
2017-09-14 17:04:02 +0200

    compiler: default entry point

--- src/compiler/parts/main.rb
+++ src/compiler/parts/main.rb
@@ -21,7 +21,7 @@ class DabCompilerFrontend
       $opt = true
       $opt = false if settings[:no_opt]
       $strip = !!settings[:strip]
-      $entry = settings[:entry]
+      $entry = settings[:entry] || 'main'
       $no_constants = settings[:no_constants]
       $no_autorelease = settings[:no_autorelease]
       $feature_reflection = settings[:with_reflection]
