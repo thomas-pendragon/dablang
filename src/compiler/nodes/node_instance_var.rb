ca973d0f5ff706f29dab740d44f1b8c72ef12cfe
Thomas Pendragon <root@thomaspendragon.com>
2017-09-30 14:25:18 +0200

    compiler: release previous value of the register when updating with an instance variable

--- src/compiler/nodes/node_instance_var.rb
+++ src/compiler/nodes/node_instance_var.rb
@@ -22,6 +22,7 @@ class DabNodeInstanceVar < DabNode
 
   def compile_as_ssa(output, output_register)
     output.comment(identifier)
+    output.print('Q_RELEASE', "R#{output_register}") if $no_autorelease
     output.printex(self, 'Q_SET_INSTVAR', "R#{output_register}", "S#{node_identifier.index}")
   end
 
