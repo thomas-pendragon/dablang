15bd7ddfab432b27c74e7da2484232f2a9bceb7a
Thomas Pendragon <root@thomaspendragon.com>
2017-09-30 14:25:01 +0200

    compiler: don’t strip member functions

--- src/compiler/processors/strip_unused_function.rb
+++ src/compiler/processors/strip_unused_function.rb
@@ -2,6 +2,7 @@ class StripUnusedFunction
   def run(node)
     return unless node.users.empty?
     return if $entry == node.identifier
+    return if node.member_function?
     node.remove!
     true
   end
