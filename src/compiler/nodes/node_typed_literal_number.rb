4b72c6c773ce0244505fe448e9fa25d131f2ffda
Thomas Pendragon <root@thomaspendragon.com>
2017-10-11 13:20:28 +0200

    compiler: remove usage of legacy PUSH_NUMBER_XXX opcodes

--- src/compiler/nodes/node_typed_literal_number.rb
+++ src/compiler/nodes/node_typed_literal_number.rb
@@ -12,21 +12,6 @@ class DabNodeTypedLiteralNumber < DabNodeLiteral
     number.to_s
   end
 
-  def compile(output)
-    case my_type.type_string
-    when 'Uint8'
-      output.print('PUSH_NUMBER_UINT8', extra_value)
-    when 'Int32'
-      output.print('PUSH_NUMBER_INT32', extra_value)
-    when 'Uint32'
-      output.print('PUSH_NUMBER_UINT32', extra_value)
-    when 'Uint64'
-      output.print('PUSH_NUMBER_UINT64', extra_value)
-    else
-      raise "cannot compile typed number [#{my_type.type_string}]"
-    end
-  end
-
   def compile_as_ssa(output, output_register)
     output.printex(self, "Q_SET_NUMBER_#{my_type.type_string.upcase}", "R#{output_register}", extra_value)
   end
