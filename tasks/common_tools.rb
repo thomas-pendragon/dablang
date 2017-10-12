f57e2583e316b42486fe95f7fc066606acb51d32
Thomas Pendragon <root@thomaspendragon.com>
2017-10-12 14:07:17 +0200

    docs: update timestamp based on mtime

--- tasks/common_tools.rb
+++ tasks/common_tools.rb
@@ -1,8 +1,17 @@
 require 'date'
 
+def format_date(date)
+  date.strftime('%Y-%m-%d')
+end
+
 def git_date(file)
   return nil unless File.exist?(file)
   value = `git log -1 --format=%cd #{file}`
   value = DateTime.parse(value)
-  value.strftime('%Y-%m-%d')
+  format_date(value)
+end
+
+def file_date(file)
+  value = File.mtime(file)
+  format_date(value)
 end
