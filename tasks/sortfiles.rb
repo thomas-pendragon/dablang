3ca3af2200c70edc81802b35666ea2f0e62845d1
Thomas Pendragon <root@thomaspendragon.com>
2017-10-26 17:49:09 +0200

    tools: auto reorder test files

--- tasks/sortfiles.rb
+++ tasks/sortfiles.rb
@@ -1,5 +1,7 @@
 require_relative '../setup.rb'
 
+autofix = ENV['AUTOFIX']&.to_i == 1
+
 directories = Dir['./test/*']
 
 directories.each do |directory|
@@ -9,6 +11,14 @@ directories.each do |directory|
   list.each_with_index do |item, index|
     start = sprintf('%04d_', index + 1)
     next if item.start_with?(start)
+    if autofix
+      new_item = item.gsub(/^\d+_/, start)
+      old_path = File.join(directory, item)
+      new_path = File.join(directory, new_item)
+      STDERR.puts "Correct: #{old_path} -> #{new_path}".red
+      File.rename(old_path, new_path)
+      next
+    end
     list.each_with_index do |preview_item, preview_index|
       next unless ((index - 2)..(index + 2)).cover?(preview_index)
       STDERR.printf("  %s %s\n", preview_index == index ? '>' : ' ', preview_item)
