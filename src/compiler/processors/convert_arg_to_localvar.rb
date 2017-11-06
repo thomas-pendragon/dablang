a29e92bf1f9ba79ed8aadeee7757810be7a7778a
Thomas Pendragon <root@thomaspendragon.com>
2017-11-06 14:53:52 +0100

    compiler: natural arg order

--- src/compiler/processors/convert_arg_to_localvar.rb
+++ src/compiler/processors/convert_arg_to_localvar.rb
@@ -1,6 +1,8 @@
 class ConvertArgToLocalvar
   def run(function)
-    function.arglist&.each_with_index do |arg, index|
+    list = function.arglist&.to_a || []
+    list.reverse.each do |arg|
+      index = list.index(arg)
       define_var = DabNodeDefineLocalVar.new(arg.identifier, DabNodeArg.new(index), arg.my_type)
       define_var.clone_source_parts_from(arg)
       function.blocks[0].pre_insert(define_var)
