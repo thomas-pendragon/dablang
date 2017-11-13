7f45736767baaa38cda02b66ba1de9f20a32c310
Thomas Pendragon <root@thomaspendragon.com>
2017-11-13 16:26:05 +0100

    disasm frontend: allow asm input

--- src/frontend/frontend_disasm.rb
+++ src/frontend/frontend_disasm.rb
@@ -32,21 +32,31 @@ def run_test(settings)
 
   data = read_test_file(input)
 
+  asm_input = data[:asm_input]
+
   options = data[:options] || ''
   with_headers = options['--with-headers']
   disasm_options = '--raw'
   disasm_options = '' if with_headers
+  disasm_options = '' if asm_input
 
   info = "Running test #{input.blue.bold} in directory #{test_output_dir.blue.bold}..."
   puts info
   FileUtils.mkdir_p(test_output_dir)
 
+  in_asm = Pathname.new(test_output_dir).join(test_prefix + File.basename(input).ext('.in.asm')).to_s
   asm = Pathname.new(test_output_dir).join(test_prefix + File.basename(input).ext('.asm')).to_s
   bin = Pathname.new(test_output_dir).join(test_prefix + File.basename(input).ext('.bin')).to_s
   out = Pathname.new(test_output_dir).join(test_prefix + File.basename(input).ext('.out')).to_s
   FileUtils.rm(out) if File.exist?(out)
 
-  extract_format_source(input, bin)
+  if asm_input
+    File.open(in_asm, 'wb') { |f| f << data[:asm_input] << "\n" }
+    assemble(in_asm, bin)
+  else
+    extract_format_source(input, bin)
+  end
+
   disassemble(bin, asm, disasm_options)
 
   expected = data[:expect].strip
