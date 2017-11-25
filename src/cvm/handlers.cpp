82095bf5168e4cfe12b678656586634907b8ea4f
Thomas Pendragon <root@thomaspendragon.com>
2017-11-25 12:45:12 +0100

    vm: fixing warnings

--- src/cvm/handlers.cpp
+++ src/cvm/handlers.cpp
@@ -6,10 +6,9 @@
 
 void handler(int sig)
 {
-    void * array[10];
-    size_t size;
+    void *array[10];
 
-    size = backtrace(array, 10);
+    int size = backtrace(array, 10);
 
     fprintf(stderr, "Error: signal %d:\n", sig);
     backtrace_symbols_fd(array, size, STDERR_FILENO);
