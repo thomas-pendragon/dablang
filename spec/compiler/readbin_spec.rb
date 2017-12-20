10a7a5723809ca59e2df04c3c6601fb7c97bd539
Thomas Pendragon <root@thomaspendragon.com>
2017-12-20 22:00:27 +0100

    compiler: parse whole binary header

--- spec/compiler/readbin_spec.rb
+++ spec/compiler/readbin_spec.rb
@@ -44,4 +44,39 @@ describe DabBinReader, readbin: true do
 
     expect(result).to eq(expected)
   end
+
+  it 'parses whole header' do
+    header = parse_bin('44 41 42 00 02 00 00 00  c0 00 00 00 00 00 00 00
+                        bd 04 00 00 00 00 00 00  05 00 00 00 00 00 00 00
+                        64 61 74 61 00 00 00 00  00 00 00 00 00 00 00 00
+                        c0 00 00 00 00 00 00 00  02 00 00 00 00 00 00 00
+                        63 6f 64 65 00 00 00 00  00 00 00 00 00 00 00 00
+                        c2 00 00 00 00 00 00 00  2c 03 00 00 00 00 00 00
+                        73 79 6d 64 00 00 00 00  00 00 00 00 00 00 00 00
+                        ee 03 00 00 00 00 00 00  53 00 00 00 00 00 00 00
+                        73 79 6d 62 00 00 00 00  00 00 00 00 00 00 00 00
+                        41 04 00 00 00 00 00 00  88 00 00 00 00 00 00 00
+                        66 75 6e 63 00 00 00 00  00 00 00 00 00 00 00 00
+                        c9 04 00 00 00 00 00 00  b4 00 00 00 00 00 00 00')
+
+    result = DabBinReader.new.parse_whole_header(header)
+
+    expected = {
+      dab: 'DAB',
+      zero: 0,
+      version: 2,
+      size_of_header: 192,
+      size_of_data: 1213,
+      sections_count: 5,
+      sections: [
+        {name: 'data', zero1: 0, zero2: 0, zero3: 0, address: 192, length: 2},
+        {name: 'code', zero1: 0, zero2: 0, zero3: 0, address: 194, length: 812},
+        {name: 'symd', zero1: 0, zero2: 0, zero3: 0, address: 1006, length: 83},
+        {name: 'symb', zero1: 0, zero2: 0, zero3: 0, address: 1089, length: 136},
+        {name: 'func', zero1: 0, zero2: 0, zero3: 0, address: 1225, length: 180},
+      ],
+    }
+
+    expect(result).to eq(expected)
+  end
 end
--- src/compiler/parts/readbin.rb
+++ src/compiler/parts/readbin.rb
@@ -8,4 +8,18 @@ class DabBinReader
     data = string.unpack('a4L<L<L<Q<Q<')
     %i[name zero1 zero2 zero3 address length].zip(data).to_h
   end
+
+  def parse_whole_header(string)
+    header = parse_header(string[0..32])
+    sections = []
+    sections_count = header[:sections_count]
+    sections_count.times do |i|
+      offset = i * 32
+      range = ((32 + offset)..(64 + offset))
+      data = string[range]
+      sections << parse_section(data)
+    end
+    header[:sections] = sections
+    header
+  end
 end
