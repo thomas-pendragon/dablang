62fb0548dffa7a1bf4fa53d340647c990634d6eb
Thomas Pendragon <root@thomaspendragon.com>
2017-12-31 12:31:15 +0100

    vm: fix name shadowing in InstructionsReader::raw_read

--- src/cvm/debug.cpp
+++ src/cvm/debug.cpp
@@ -134,12 +134,12 @@ struct InstructionsReader : public BaseReader
 
     virtual uint64_t raw_read(void *buffer, uint64_t size) override
     {
-        auto data       = _data;
+        auto data_ptr   = _data;
         auto length     = _length;
         auto offset     = position();
         auto max_length = std::min(size, length - offset);
 
-        memcpy(buffer, data + offset, (size_t)max_length);
+        memcpy(buffer, data_ptr + offset, (size_t)max_length);
 
         return max_length;
     }
