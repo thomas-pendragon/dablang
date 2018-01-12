1625a70bf62a62234ca7f468ec920dc87b0ed3eb
Thomas Pendragon <root@thomaspendragon.com>
2018-01-12 18:25:13 +0100

    compiler: function stubs (from other modules)

---
+++ src/compiler/nodes/node_function_stub.rb
@@ -0,0 +1,22 @@
+require_relative 'node.rb'
+
+class DabNodeFunctionStub < DabNode
+  attr_reader :identifier
+
+  def initialize(identifier, _arglist = nil)
+    super()
+    @identifier = identifier
+  end
+
+  def concreteified?
+    true
+  end
+
+  def compile_body(*args); end
+
+  def compile_definition(*args); end
+
+  def extra_dump
+    identifier
+  end
+end
