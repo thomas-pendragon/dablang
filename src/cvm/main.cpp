03a968158aa4b906a83b642fe8bcb0ffa8cf684e
Tomasz Pędraszewski <t.pedraszewski@28byteslater.com>
2023-03-19 00:24:11 +0100

    syscall [2]

--- src/cvm/main.cpp
+++ src/cvm/main.cpp
@@ -54,7 +54,31 @@ DabClass &DabVM::get_class(dab_class_t index)
 
 void DabVM::kernel_define_method(dab_register_t out_reg, std::vector<dab_register_t> reglist)
 {
-    (void)reglist;
+//                                  LOAD_METHOD R0, S1
+// /* "foo"        */               LOAD_STRING R1, _DATA + 0, 3
+// /* DEFINE_METHO */               SYSCALL RNIL, 5, R1, R0
+
+
+    assert(reglist.size() == 2);
+
+    auto method = register_get(reglist[0]);
+    auto name = register_get(reglist[1]);
+
+    fprintf(stderr, "VM: define_method\n");
+    fprintf(stderr, "VM: method: "); 
+    method.dump(stderr);
+    fprintf(stderr, "\n");
+    fprintf(stderr, "VM: name:   "); 
+    name.dump(stderr);
+    fprintf(stderr, "\n");
+
+    assert(method.data.type == TYPE_METHOD);
+    assert(name.data.type == TYPE_LITERALSTRING); // dynamic?
+
+    auto method_symbol = method.data.fixnum;
+    auto method_name = name.string();
+
+    fprintf(stderr, "VM: define_method %x (%p) as '%s'\n", (int)method_symbol, (void*)method_symbol, method_name.c_str());
 
     register_set(out_reg, nullptr);
 }
