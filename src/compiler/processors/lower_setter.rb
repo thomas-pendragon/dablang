62eff6dc3b192b9c9215a3f616dc69e245216e1a
Tomasz Pędraszewski <t.pedraszewski@28byteslater.com>
2023-03-19 17:30:54 +0100

    rubocop Style/CaseLikeIf

--- src/compiler/processors/lower_setter.rb
+++ src/compiler/processors/lower_setter.rb
@@ -2,24 +2,25 @@ class LowerSetter
   def run(node)
     reference = node.reference
     value = node.value
-    if reference.is_a? DabNodeReferenceIndex
+    case reference
+    when DabNodeReferenceIndex
       base = reference.base.compiled
       index = reference.index
       setcall = DabNodeInstanceCall.new(base, :[]=, [index, value], nil)
       node.replace_with!(setcall)
       true
-    elsif reference.is_a? DabNodeReferenceMember
+    when DabNodeReferenceMember
       base = reference.base.compiled
       name = reference.name
       setcall = DabNodeInstanceCall.new(base, "#{name}=".to_sym, [value], nil)
       node.replace_with!(setcall)
       true
-    elsif reference.is_a? DabNodeReferenceInstVar
+    when DabNodeReferenceInstVar
       base = reference.compiled
       setcall = DabNodeSetInstVar.new(base.identifier, value)
       node.replace_with!(setcall)
       true
-    elsif reference.is_a? DabNodeReferenceLocalVar
+    when DabNodeReferenceLocalVar
       setcall = DabNodeSetLocalVar.new(reference.name, value)
       node.replace_with!(setcall)
       true
