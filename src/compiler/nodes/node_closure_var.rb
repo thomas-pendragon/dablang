3741c5028925f933da378fbf703332ef14116a4a
Tomasz Pędraszewski <t.pedraszewski@28byteslater.com>
2023-03-25 21:07:08 +0100

    block class [4]

--- src/compiler/nodes/node_closure_var.rb
+++ src/compiler/nodes/node_closure_var.rb
@@ -2,6 +2,7 @@ require_relative 'node'
 
 class DabNodeClosureVar < DabNode
   attr_reader :index
+  lower_with :to_instvar
 
   def initialize(index)
     super()
@@ -14,5 +15,13 @@ class DabNodeClosureVar < DabNode
 
   def compile_as_ssa(output, output_register)
     output.print('LOAD_CLOSURE', "R#{output_register}", index)
+  end
+
+  def to_instvar
+    data = DabNodeInstanceVar.new("@closure")
+    lindex = DabNodeLiteralNumber.new(@index)
+    op = DabNodeInstanceCall.new(data, "[]", [lindex], nil)
+    replace_with!(op)
+    true
   end
 end
