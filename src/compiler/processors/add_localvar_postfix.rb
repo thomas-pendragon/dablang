00646eae189ee1eaa68ddfc987dd94d3f5b25e9c
Tomasz Pędraszewski <t.pedraszewski@28byteslater.com>
2023-03-26 23:21:25 +0200

    remove log

--- src/compiler/processors/add_localvar_postfix.rb
+++ src/compiler/processors/add_localvar_postfix.rb
@@ -1,12 +1,5 @@
 class AddLocalvarPostfix
   def run(node)
-    xx = node.function.identifier == 'call'
-    if xx
-      errap ['AddLocalvarPostfix', node, 'source', node.source_file, node.source_line, 'parent', node.function.identifier]
-      node.function.dump
-      err '-' * 80
-    end
-
     return if node.identifier['#']
 
     fun_index = 1
@@ -17,20 +10,10 @@ class AddLocalvarPostfix
       fun_index += 1
     end
 
-    if xx
-      errap ['all users:']
-      node.all_users.each(&:dump)
-    end
-
     node.all_users.each do |user|
       user.identifier = new_id
     end
 
-    if xx
-      err 'NOW:'
-      node.function.dump
-      err "\n\n\n#{('~' * 80).blue}"
-    end
     true
   end
 end
