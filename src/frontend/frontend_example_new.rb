f18b8a958234473294daedb7d9c5b2a6088b9361
Tomasz Pędraszewski <t.pedraszewski@28byteslater.com>
2023-03-30 18:21:35 +0200

    multiexample runner [1]

--- src/frontend/frontend_example_new.rb
+++ src/frontend/frontend_example_new.rb
@@ -36,33 +36,39 @@ class DabExampleSpec
     puts info
     FileUtils.mkdir_p(test_output_dir)
 
+    vmfiles = []
+
     levels = Dir.glob("#{test_dir}/level*").sort
     levels.each_with_index do |level_dir, level|
+      is_final = level == (levels.count - 1)
+
       info = "Running test ##{input} #{test_dir.blue.bold} level #{level.to_s.bold.blue} (#{level_dir.yellow})"
       puts info
 
-      level_base = test_prefix + input.to_s + '_level' + level.to_s
+      level_base = "#{test_prefix}#{input}_level#{level}"
 
-      dab = Dir.glob(level_dir + '/*.dab')
-      asm = Pathname.new(test_output_dir).join(level_base.ext('.dabca')).to_s
-      bin = Pathname.new(test_output_dir).join(level_base.ext('.dabcb')).to_s
-      out = Pathname.new(test_output_dir).join(level_base.ext('.out')).to_s
+      dab = Dir.glob("#{level_dir}/*.dab")
+      basepath = Pathname.new(test_output_dir)
+      asm = basepath.join(level_base.ext('.dabca')).to_s
+      bin = basepath.join(level_base.ext('.dabcb')).to_s
+      out = basepath.join(level_base.ext('.out')).to_s
+      vmo = basepath.join(level_base.ext('.vm')).to_s
+      vmoa = basepath.join(level_base.ext('.vm.dabca')).to_s
 
       stdlib_path = File.expand_path("#{File.dirname(__FILE__)}/../../stdlib/")
       stdlib_glob = "#{stdlib_path}/*.dab"
       stdlib_files = Dir.glob(stdlib_glob)
 
       options = ''
-      run_options = options
+      run_options = "--entry=level#{level}"
+      run_options += ' --output=dumpvm' unless is_final
+      run_options += ' --verbose'
 
       compile_dab_to_asm((dab + stdlib_files).compact, asm, options)
       assemble(asm, bin)
+      execute(bin, vmo, run_options)
+      disassemble(vmo, vmoa, '--with-headers')
 
-      # if $dont_run_example
-      #   File.open(out, 'wb') { |f| f << '1' }
-      # else
-      #   execute(bin, run_options)
-      # end
       raise 'a'
     end
   end
