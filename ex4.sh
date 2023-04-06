1a033f9f120ad345cbd83d297d4102acfd2601e4
Tomasz Pędraszewski <t.pedraszewski@28byteslater.com>
2023-04-06 19:50:41 +0200

    orm with class vars (1)

---
+++ ex4.sh
@@ -0,0 +1,1 @@
+rake example[4] >/dev/null 2>/dev/null; ./bin/cvm --entry=level2 --xverbose ./tmp/example_4/example_4_level0.vm ./tmp/example_4/example_4_level1.vm ./tmp/example_4/example_4_level2.dabcb 2>/dev/null
