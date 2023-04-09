a81290728d78d9969939ffa3db67eefd8288784d
Tomasz Pędraszewski <t.pedraszewski@28byteslater.com>
2023-04-09 21:05:04 +0200

    orm kinda works!

--- ex4.sh
+++ ex4.sh
@@ -1,1 +1,7 @@
-rake example[4] >/dev/null 2>/dev/null; ./bin/cvm --entry=level2 --xverbose ./tmp/example_4/example_4_level0.vm ./tmp/example_4/example_4_level1.vm ./tmp/example_4/example_4_level2.dabcb 2>/dev/null
+rake example[4] >/dev/null || exit 1 # 2>/dev/null
+echo LEVEL 0
+./bin/cvm --entry=level1 --xverbose ./tmp/example_4/example_4_level0.dabcb 2>/dev/null
+echo LEVEL 1
+./bin/cvm --entry=level1 --xverbose ./tmp/example_4/example_4_level0.vm ./tmp/example_4/example_4_level1.dabcb 2>/dev/null
+echo LEVEL 2
+./bin/cvm --entry=level2 --xverbose ./tmp/example_4/example_4_level0.vm ./tmp/example_4/example_4_level1.vm ./tmp/example_4/example_4_level2.dabcb 2>/dev/null
