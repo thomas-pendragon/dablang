4f9f360f122eadb119029d1d759928f4d64930a3
Tomasz Pędraszewski <t.pedraszewski@28byteslater.com>
2024-10-09 12:58:08 +0200

    extra teest

--- src/cvm/c_export.cpp
+++ src/cvm/c_export.cpp
@@ -16,6 +16,51 @@ static void load_address(unsigned char *code_ptr, uintptr_t value)
 typedef int (*int_fun_handler_ptr)(void *);
 typedef int (*int_fun_ptr)();
 
+void simple_x86_test() {
+    fprintf(stderr,"x86 test...\n");
+    unsigned char code[] = {
+        0x48, 0xB8,                       // mov rax, imm64
+        0x42, 0x00, 0x00, 0x00,     // Set RAX to 0x0000000000000042 (little-endian representation)
+        0x00, 0x00, 0x00, 0x00,
+        0xC3                              // ret (return from the function)
+    };
+
+    for (int i = 0 ; i < (int)sizeof(code); i++)
+    {
+        fprintf(stderr, "%02x ", code[i]);
+    }
+    fprintf(stderr, "\n");
+
+    // Allocate executable memory
+    void *exec_mem = mmap(NULL, sizeof(code), PROT_READ | PROT_WRITE | PROT_EXEC,
+                          MAP_PRIVATE | MAP_ANON, -1, 0);
+    if (exec_mem == MAP_FAILED) {
+        perror("mmap");
+        return;
+    }
+    fprintf(stderr,"x86 test...%p\n",exec_mem);
+
+    // Copy machine code to the allocated executable memory
+    memcpy(exec_mem, code, sizeof(code));
+
+    // Cast the memory to a function pointer
+    typedef int (*func_ptr)();
+    func_ptr func = (func_ptr)exec_mem;
+    fprintf(stderr,"x86 test... func %p\n",(void*)func);
+
+    // Call the generated function
+    int result = func();
+
+    fprintf(stderr,"x86 test... result %x\n", result);
+
+    // Output the result
+    fprintf(stderr, "Result in RAX: 0x%lx\n", (unsigned long)result);
+    assert(result==0x42);
+
+    // Free the allocated memory (if needed, depending on your system's conventions)
+    munmap(exec_mem, sizeof(code));
+}
+
 static int_fun_ptr create_dynamic_func(int_fun_handler_ptr func_template, void *literal)
 {
     fprintf(stderr, "create_dynamic_func(func=%p,literal=%p)\n",(void*)func_template,(void*)literal);
@@ -63,7 +108,7 @@ struct DabIntFunctionHandler
     DabValue method;
 };
 
-static int call_dab_int_function(void *ptr)
+int call_dab_int_function(void *ptr)
 {
     fprintf(stderr, "func_template called with ptr = %p\n", ptr);
     auto castPtr     = (DabIntFunctionHandler *)ptr;
@@ -77,6 +122,8 @@ static int call_dab_int_function(void *ptr)
 
 void DabVM::kernel_c_export(dab_register_t out_reg, std::vector<dab_register_t> reglist)
 {
+    simple_x86_test();
+
     assert(reglist.size() == 1);
 
     DabValue method = register_get(reglist[0]);
